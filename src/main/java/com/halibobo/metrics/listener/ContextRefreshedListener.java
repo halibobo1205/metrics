45291a2a9b4696411afe357624585af6c1b5f1ef
halibobo1205 <halibobo1205@gmail.com>
2022-01-21 11:16:04 +0800

    add ContextRefreshedListener

---
+++ src/main/java/com/halibobo/metrics/listener/ContextRefreshedListener.java
@@ -0,0 +1,19 @@
+package com.halibobo.metrics.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Component
+@Slf4j(topic = "listener")
+public class ContextRefreshedListener implements ApplicationListener<ContextRefreshedEvent> {
+
+
+  @Override
+  public void onApplicationEvent(ContextRefreshedEvent event) {
+    if (event.getApplicationContext().getParent() == null) {
+      //do somethings
+    }
+  }
+}
